da18250973c234a2b76d763dce323ca149c2f2fc
Oliver Lenehan <oliver@mytanic.net>
2019-01-25 14:59:31 +1100

    xmltest

---
+++ data.js
@@ -0,0 +1,13 @@
+/*
+    https://lendahand16.github.io
+    http://www.bom.gov.au/ntc/IDO59001/IDO59001_2019_NSW_TP007.xml
+*/
+
+let xhttp = new XMLHttpRequest();
+xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+        console.log(this.responseXML.getElementsByTagName("forecast-period")[24]);
+    }
+}
+xhttp.open("GET", "http://www.bom.gov.au/ntc/IDO59001/IDO59001_2019_NSW_TP007.xml", true);
+xhttp.send();
